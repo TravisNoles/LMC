8b4d256292b42030af1caa94f10a7aefd6bfed3b
TravisNoles <noles.travis@gmail.com>
2015-02-23 18:16:54 -0500

    commit ch. 9.3 vb

---
+++ Ch9.3/Ch9.3/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Default.aspx.vb" Inherits="Ch9._3.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Username: 
+        <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+        <br />
+        Password: 
+        <asp:TextBox ID="txtPass" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        Confirm Password:&nbsp; <asp:TextBox ID="txtPassConfirm" runat="server" TextMode="Password"></asp:TextBox>
+        <br />
+        <asp:Label ID="lbl_error" runat="server"></asp:Label>
+        <br />
+        <asp:Button ID="cmdSubmit" runat="server" Text="Submit" />
+        <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ch9.3/Ch9.3/Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿Public Class Index
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtUsername.TextChanged
+
+    End Sub
+
+
+    Protected Sub CmdSubmit_Click(sender As Object, e As EventArgs) Handles cmdSubmit.Click
+
+        'Check for valid password field (must match)
+        If (txtPass.Text = txtPassConfirm.Text) Then
+            lbl_error.Text = "Welcome," + txtUsername.Text + "!"
+        End If
+
+
+        ' If passwords are not equal !=
+        If (txtPass.Text <> txtPassConfirm.Text) Then
+            txtPass.Text = ""
+            txtPassConfirm.Text = ""
+            lbl_error.Text = "Passwords don't match."
+        End If
+
+
+
+
+
+    End Sub
+
+End Class
